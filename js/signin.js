24b783b17b1f2fea2986a692da4c8b9907d59680
Kuo, Tech (tk685f) <tk685f@us.att.com>
2015-07-20 11:17:20 -0500

    sign in page hopefully working

--- js/signin.js
+++ js/signin.js
@@ -1,22 +1,26 @@
-angular.module('firstModule', ['ui.router', 'ui.bootstrap'])
+angular.module('myApp')
     .controller('SignInController',  ['$scope','$rootScope','$location', function ($scope, $rootScope, $location) {
 
         $scope.username = '';
-        $scope.pw = '';
+        $scope.password = '';
         $scope.loginError = false;
 
        $scope.signin = function(){
-            if ($scope.username && $scope.pw){
+            if ($scope.username && $scope.password){
+
                 var UserClass = Parse.Object.extend("Users");
                 var query = new Parse.Query(UserClass);
                 
                 // search Parse for login credentials
                 query.find( {
                     success: function(results) {
-                        for (var i=0; i<results.length; i++) {
+                        for (var i=0; i < results.length; i++) {
+
+
                             if (results[i].get("username") == $scope.username &&
                                 results[i].get("password") == $scope.password) {
                                     
+
                                     // set 'session' for current user
                                     $rootScope.currentUser = $scope.username;
                                    
@@ -25,13 +29,16 @@ angular.module('firstModule', ['ui.router', 'ui.bootstrap'])
                                     localStorage.currentUser = $scope.username;
 
                                     // redirect to index
-                                    $location.path('/');
+                                    $location.path('/myEvents');
+                            }
+                            else {
+                                $scope.loginError = true;
                             }
                         }
                     }, error: function(results) {
-                        $scope.loginError = true;
+                        alert("Error with Logging In.")
                     }
                 });
             }
         };
-    }])
+    }]);
